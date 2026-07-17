bd6c8598fa2f01085e98c1cdc81805b1878dbd9b
Jazreil Jaron Sabog <sabogjazreiljaron@gmail.com>
2026-07-17 10:07:52 +0800

    Add PHPMailer

--- server/config.php
+++ server/config.php
@@ -10,6 +10,5 @@ return [
     'smtp_host'     => getenv('SMTP_HOST') ?: 'smtp.gmail.com',
     'smtp_port'     => getenv('SMTP_PORT') ?: 587,
     'smtp_user'     => getenv('SMTP_USER') ?: 'denr.ojt.geostratum@gmail.com',
-    'smtp_password' => getenv('SMTP_PASS') ?: 'lfdmrqjyzdlsnstu
-',
+    'smtp_password' => getenv('SMTP_PASS') ?: 'lfdmrqjyzdlsnstu',
 ];
